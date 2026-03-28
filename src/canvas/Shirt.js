705f1a117b756e8cdf04cb9d2dbd315c386fd6b7
Vazgen <vouqreel@gmail.com>
2026-03-29 01:04:14 +0500

    remove unused THREE import

--- src/canvas/Shirt.js
+++ src/canvas/Shirt.js
@@ -3,8 +3,6 @@ import { easing } from "maath";
 import { useSnapshot } from "valtio";
 import { useFrame } from "@react-three/fiber";
 import { Decal, useGLTF, useTexture } from "@react-three/drei";
-import * as THREE from "three";
-
 import state from "../store";
 
 const Shirt = () => {
